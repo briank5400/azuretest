efeb86d5bc37db8435a3abe3f04d35a331d00999
briank5400 <briank5400@users.noreply.github.com>
2023-01-04 14:11:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,15 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroupCloud"
   location = "westus2"
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.westus2
+  resource_group_name = azurerm_resource_group.rg
+  address_space       = ["10.2.0.0/16"]
+  dns_servers         = ["10.2.0.4", "10.2.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.2.1.0/24"
+  }
